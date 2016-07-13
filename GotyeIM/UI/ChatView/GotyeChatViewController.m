28b1c9d1f57741657be387000a3e2143ed580356
guoranliaoran <774610599@qq.com>
2016-07-13 14:03:29 +0800

    删除群消息的过滤条件
    删除群消息的过滤条件

--- GotyeIM/UI/ChatView/GotyeChatViewController.m
+++ GotyeIM/UI/ChatView/GotyeChatViewController.m
@@ -136,10 +136,10 @@ UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UIN
     if ([RedpacketMessageModel isRedpacketRelatedMessage:ext]) {
         if ([RedpacketMessageModel isRedpacketTakenMessage:ext])    {
             // 由于没有透传消息 如果群红包，A发的，A打开，others收到消息，others删除消息
-            if ([ext[@"money_sender_id"] isEqualToString:ext[@"money_receiver_id"]]) {
-                [GotyeOCAPI deleteMessage:chatTarget msg:message];
-                return;
-            }
+            // if ([ext[@"money_sender_id"] isEqualToString:ext[@"money_receiver_id"]]) {
+            //     [GotyeOCAPI deleteMessage:chatTarget msg:message];
+            //     return;
+            // }
             // 由于没有透传消息 如果群红包，A发的，B打开，other收到消息，除了A之外的others删除
             if (![ext[@"money_sender_id"] isEqualToString:loginUser.name]) {
                 [GotyeOCAPI deleteMessage:chatTarget msg:message];
